2b89d10aab1b39ca6467b36cfda471a39c7cd58e
duhauda <wuyilong1758468@163.com>
2023-10-24 10:24:17 +0800

    第10.24次

--- src/main/java/com/maccwok/xd/Controller/LoginController.java
+++ src/main/java/com/maccwok/xd/Controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.maccwok.xd.Controller;
 
 
+import cn.hutool.core.util.StrUtil;
 import com.maccwok.xd.dto.LoginDTO;
+import com.maccwok.xd.dto.RegisterDTO;
+import com.maccwok.xd.entity.UserEntity;
+import com.maccwok.xd.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +16,29 @@ import java.util.ArrayList;
 
 @RestController
 public class LoginController {
+    @Autowired private UserService userService;
     @PostMapping("login")
     public  String login(@RequestBody LoginDTO loginDTO){
         System.out.println(loginDTO);
-        return "注册成功";
+        return "11";
     }
 
-    @GetMapping("register")
-    public String register(){
+    @PostMapping("register")
+    public String register(@RequestBody RegisterDTO registerDTO){
+    String account = registerDTO.getAccount();
+    String password = registerDTO.getPassword();
+    String password1 = registerDTO.getPassword1();
+       if( StrUtil.isEmpty(account)||StrUtil.isEmpty(password)||StrUtil.isEmpty(password1)) {
+           return "数据校验不通过";
+       }
+        if(!password1.equals(password)){
+            return "第一次密码与第二次密码不一致";
+        }
+        UserEntity userEntity = new UserEntity();
+        userEntity.setAccount(account);
+        userEntity.setPassword(password);
+        userService.save(userEntity);
+        System.out.println(registerDTO);
         return "register";
     }
     @GetMapping("getFruit")
